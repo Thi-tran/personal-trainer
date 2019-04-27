8dad06e7616aa17960e831a315ab852df86e419a
Thithran <thi.tran.fi@gmail.com>
2019-04-27 23:58:46 +0300

    not giving firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase';
 
+// Input your Firebase data here 
 const config = {
-    apiKey: "AIzaSyAJGL91I_2fDFAWh-fXrl8Q8ecKxFKrz5c",
-    authDomain: "no-loan-pay.firebaseapp.com",
-    databaseURL: "https://no-loan-pay.firebaseio.com",
-    projectId: "no-loan-pay",
-    storageBucket: "no-loan-pay.appspot.com",
-    messagingSenderId: "760952382897"  
 };
 
 firebase.initializeApp(config);
